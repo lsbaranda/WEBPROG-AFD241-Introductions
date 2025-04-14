af2894ae5bf9e0be511de694441d9989fce82c94
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-04-13 19:56:47 -0700

    js DOM demo

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
+    document.getElementById("loc").style.color="red";
+    document.getElementById("loc").style.fontFamily = "Arial";
+    document.getElementById("loc").style.fontSize = "larger";
 }
