c426ef576eb0b69e05233017341679fb62dc1ed4
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-04-30 16:14:52 -0700

    js updated

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML="Paragraph changed.";
+    document.getElementById("demo").innerHTML="Paragraph updated.";
     document.getElementById("loc").style.color="red";
     document.getElementById("loc").style.fontFamily = "Arial";
     document.getElementById("loc").style.fontSize = "larger";
