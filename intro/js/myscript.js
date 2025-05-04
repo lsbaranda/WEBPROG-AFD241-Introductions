09f7f697ab90bd9660040c052f09820493c7fa14
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-04 00:11:05 -0700

    image click color fix

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,12 +1,24 @@
 function myWelder() {
-    document.getElementById("clickWelder").style.backgroundColor="red";
+    document.getElementById("clickWelder").style.backgroundColor="orangered";
+    document.getElementById("clickOffroad").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickHike").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickRadio").style.backgroundColor="rgba(33, 33, 33, 0.625)";
 }
 function myOffroad() {
-    document.getElementById("clickOffroad").style.backgroundColor="red";
+    document.getElementById("clickWelder").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickOffroad").style.backgroundColor="orangered";
+    document.getElementById("clickHike").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickRadio").style.backgroundColor="rgba(33, 33, 33, 0.625)";
 }
 function myHike() {
+    document.getElementById("clickWelder").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickOffroad").style.backgroundColor="rgba(33, 33, 33, 0.625)";
     document.getElementById("clickHike").style.backgroundColor="orangered";
+    document.getElementById("clickRadio").style.backgroundColor="rgba(33, 33, 33, 0.625)";
 }
 function myRadio() {
-    document.getElementById("clickRadio").style.backgroundColor="red";
+    document.getElementById("clickWelder").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickOffroad").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickHike").style.backgroundColor="rgba(33, 33, 33, 0.625)";
+    document.getElementById("clickRadio").style.backgroundColor="orangered";
 }
