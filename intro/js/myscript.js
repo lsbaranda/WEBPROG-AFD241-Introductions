b0ace710f14fe57e7f92e17851e48eb4a86645c8
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-03 23:45:56 -0700

    click test fix

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,3 +1,6 @@
 function myOffroad() {
-    document.getElementById("clickOffroad").style.backgroundColor=rgba(255, 68, 0, 0.500);
+    document.getElementById("clickOffroad").style.backgroundColor= rgba(255, 68, 0, 0.500);
+}
+function myHike() {
+    document.getElementById("clickHike").style.backgroundColor= rgba(255, 68, 0, 0.500);
 }
