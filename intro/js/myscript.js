f3ab18889f770b1978d9ca701064c182f47cc1f8
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-03 23:50:53 -0700

    more click fix

--- intro/js/myscript.js
+++ intro/js/myscript.js
@@ -1,6 +1,12 @@
+function myWelder() {
+    document.getElementById("clickWelder").style.backgroundColor= rgba(255, 68, 0, 0.500);
+}
 function myOffroad() {
     document.getElementById("clickOffroad").style.backgroundColor= rgba(255, 68, 0, 0.500);
 }
 function myHike() {
     document.getElementById("clickHike").style.backgroundColor= rgba(255, 68, 0, 0.500);
+}
+function myRadio() {
+    document.getElementById("clickRadio").style.backgroundColor= rgba(255, 68, 0, 0.500);
 }
